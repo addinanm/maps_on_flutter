fdc262fe7ee7fb5a39f8ac44f096369bcb324eeb
addina nurul <addinanurulmawaddah39@gmail.com>
2022-09-14 12:13:54 +0700

    maps titik rumah

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
 # maps_api
 
-A new Flutter project.
+## Addina Nurul Mawaddah
 
-## Getting Started
+### Praktikum
+### Menempatkan Google di Peta
+![Screenshot Hello World](images/Praktikum.jpg)<br>
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Menampilkan titik rumah
+![Screenshot Hello World](images/Tugas.jpg)<br>
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'src/locations.dart' as locations;
+import 'dart:async';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MyAppMaps(),
+    );
+  }
+}
 
+class MyAppMaps extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
-      _markers.clear();
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
-          infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
-          ),
-        );
-        _markers[office.name] = marker;
-      }
-    });
-  }
+class _MyAppState extends State<MyAppMaps> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  CameraPosition _mapInitialPosition = CameraPosition(
+    target: LatLng(-7.716402, 112.101896),
+    zoom: 18.0,
+  );
+
+  CameraPosition _anotherPosition = CameraPosition(
+    target: LatLng(-6.3753924, 106.9271528),
+    zoom: 15.0,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Office Locations'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 2,
-          ),
-          markers: _markers.values.toSet(),
-        ),
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _mapInitialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _moveCamera,
+        label: Text('GO'),
+        icon: Icon(Icons.directions_walk),
       ),
     );
+  }
+
+  Future<void> _moveCamera() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_anotherPosition));
   }
 }
